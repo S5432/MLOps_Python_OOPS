9bb384da27418a94de52675f970b15c718e5aebc
Subhash <sprajapat160599@gmail.com>
2024-12-30 22:28:21 +0530

    2 - add signup and signin method

--- oops_project_1.py
+++ oops_project_1.py
@@ -14,9 +14,9 @@ class chatter:
                            5 Press any other key to exit  """)
         
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
@@ -28,6 +28,25 @@ class chatter:
     def signup(self):
         email = input("Enter your email here -->")
         password = input("Setup your password here -->")
+        self.username = email
+        self.password = password
+        print("You have signed up successfully!!!")
+        print("\n")
+        self.menu()
+    
+    def signin(self):
+        if self.username == "" and self.password == "":
+            print("Please Signup first by pressing 1 in the main menu")
+        else:
+            uname = input("Enter your email/username here ->")
+            pwd = input("Enter your password here ->")
+            if self.username ==uname and self.password ==pwd:
+                print("You have signed in successfully!!")
+                self.loggedin = True
+            else:
+                print("Please enter the correct credentials..")
+        print("\n")
+        self.menu()
 
 obj = chatter()
         
